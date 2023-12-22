f4bfb8545903494102ce92c278b28eefd8b351f6
Malcolm <mholliday6611@gmail.com>
2023-12-22 12:17:00 -0500

    Add bear to forest scene

--- scripts/Chase.gd
+++ scripts/Chase.gd
@@ -1,15 +1,13 @@
 extends Node2D
 
-@onready var Player
-
+@onready var player_animation = $Player/AnimationPlayer
+@onready var bear_animation = $Bear/AnimationPlayer
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Player = self.get_node("Player")
-	Player.get_node('PlayerAnimation').play("run")
+	player_animation.play("run")
+	bear_animation.play("chase")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-	
