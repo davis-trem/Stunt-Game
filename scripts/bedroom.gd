ccda7fadd40c7519c7d155a6b940043e317bbc44
davis.trem <davis.trem@gmail.com>
2024-01-22 19:51:39 -0500

    update aspect ratio

--- scripts/bedroom.gd
+++ scripts/bedroom.gd
@@ -1,10 +1,18 @@
 extends Node
 
+@onready var job_list_container: TextureRect = $JobListContainer
+@onready var money_amount: Label = $MoneyAmount
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	self.get_node("HUD").get_node("MoneyAmount").text = "Money $" + str(GameState.player_stats.money)
+	money_amount.text = "Money $" + str(GameState.player_stats.money)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+
+func _on_display_job_button_pressed() -> void:
+	job_list_container.show()
